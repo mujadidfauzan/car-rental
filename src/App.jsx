d5945bd92ee795184b59fbe498cafea4f560b3c8
mujadidfauzan <kaptozora@gmail.com>
2025-01-10 20:33:36 +0700

    animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const App = () => {
   const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light');
@@ -16,10 +18,20 @@ const App = () => {
     }
   });
 
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: 'ease-in-sine',
+      delay: 100,
+    });
+    AOS.refresh();
+  });
+
   return (
     <div>
       <Navbar theme={theme} setTheme={setTheme} />
-      <Hero />
+      <Hero theme={theme} />
     </div>
   );
 };
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import carPng from '../../assets/car.png';
+import yellowCar from '../../assets/banner-car.png';
 
-const Hero = () => {
+const Hero = ({ theme }) => {
   return (
-    <div className="dark:bg-black dark:text-white duration-300">
+    <div className="dark:bg-black dark:text-white duration-300 relative -z-20">
       <div className="container min-h-[620px] flex">
         <div className="grid grid-cols-1 sm:grid-cols-2 place-items-center">
-          <div className="order-1 sm:order-2">
-            <img src={carPng} alt="car" />
+          <div data-aos="zoom-in" className="order-1 sm:order-2">
+            <img src={theme == 'dark' ? carPng : yellowCar} alt="car" className="relative -z-10 max-h-[620px] sm:scale-125 drop-shadow-[2px_20px_6px_rgba(0,0,0,0.50)]" />
           </div>
-          <div className="order-2 sm:order-1">
-            <p className="text-primary text-2xl font-serif">Effortless</p>
-            <h1 className="text-5xl lg:text-7xl font-serif font-semibold">Car Rental</h1>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          <div className="order-2 sm:order-1 space-y-5 sm:pr-32">
+            <p data-aos="fade-up" data-aos-delay="600" className="text-primary text-2xl font-serif">
+              Effortless
+            </p>
+            <h1 data-aos="fade-up" data-aos-delay="600" className="text-5xl lg:text-7xl font-serif font-semibold">
+              Car Rental
+            </h1>
+            <p data-aos="fade-up" data-aos-delay="1000">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            </p>
+            <button data-aos="fade-up" data-aos-duration="1500" className="bg-primary px-6 py-2 rounded-md text-black hover:bg-primary/80 duration-150">
+              Get Started
+            </button>
           </div>
         </div>
       </div>
